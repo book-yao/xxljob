899d038c10cb05c300e355cd2200b0f8e85e1a03
jiangwangfa <jiangwangfa1@supconit.com>
2025-05-21 15:51:00 +0800

    feat: 子任务参数为空才打印日志

--- xxl-job-admin/src/main/java/com/xxl/job/admin/core/complete/XxlJobCompleter.java
+++ xxl-job-admin/src/main/java/com/xxl/job/admin/core/complete/XxlJobCompleter.java
@@ -81,9 +81,11 @@ public class XxlJobCompleter {
                         if(childExecutorParam != null && childExecutorParam.isParamApply()){
                             executorParam = childExecutorParam.getParam();
                         }
-                        logger.info("父任务 handler:{}, 父任务:{}, 父任务logId:{}, 触发子任务:{}, 执行参数:{}, 父任务参数:{}, 父任务latestHandleMsg:{}",
-                                xxlJobLog.getExecutorHandler(), xxlJobLog.getJobId(), xxlJobLog.getId(), childJobId, executorParam,
-                                xxlJobLog.getExecutorParam(), xxlJobLog.getLatestHandleMsg());
+                        if(executorParam == null){
+                            logger.info("父任务 handler:{}, 父任务:{}, 父任务logId:{}, 触发子任务:{}, 执行参数:{}, 父任务参数:{}, 父任务latestHandleMsg:{}",
+                                    xxlJobLog.getExecutorHandler(), xxlJobLog.getJobId(), xxlJobLog.getId(), childJobId, executorParam,
+                                    xxlJobLog.getExecutorParam(), xxlJobLog.getLatestHandleMsg());
+                        }
                         JobTriggerPoolHelper.trigger(childJobId, TriggerTypeEnum.PARENT, -1, null, executorParam, null);
                         ReturnT<String> triggerChildResult = ReturnT.SUCCESS;
 
